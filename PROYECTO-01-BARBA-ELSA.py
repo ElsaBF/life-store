e621ba14308909ac0ab8f6a284c4e2483a6f832e
ElsaBF <elsaxbarba@gmail.com>
2022-02-06 23:25:10 -0600

    Comentarios

--- PROYECTO-01-BARBA-ELSA.py
+++ PROYECTO-01-BARBA-ELSA.py
@@ -1,17 +1,21 @@
+# Del archivo users.py importamos la lista llamada users_list
 from users import users_list
 
+# Definimos una función para realizar el login
 def login():
+    mensaje_bienvenida = "Bienvenido, por favor ingresa tus credenciales para acceder al sistema."
+    print(mensaje_bienvenida)
     user = input("Escriba el nombre de usuario: ")
     password = input("Escriba la contraseña: ")
     msg = "Usuario inexistente"
-    
+
     for u in users_list:
         if user == u[1]:
             if password == u[2]:
                 # Acceso aprobado para el usuario
                 print("Acceso permitido")
                 msg = "El usuario " + user + " inició sesión exitosamente"
-                anlisis()
+                ##### Aquí va el nombre de la función a la que acceder en caso de que las credenciales sean válidas
             else:
                 print("Acceso denegado")
                 msg = "Contraseña incorrecta"
@@ -19,4 +23,4 @@ def login():
     print(msg)
     
 if __name__ == "__main__":
-    anlisis()
+    login()
