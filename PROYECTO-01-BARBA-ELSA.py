47fb54a574f2126e0a43800b76aaf3ec67e2e2c1
ElsaBF <elsaxbarba@gmail.com>
2022-02-09 18:35:39 -0600

    Menores ventas
    Agregar una busqueda para los objetos menos vendidos por categorías. Closes #9

--- PROYECTO-01-BARBA-ELSA.py
+++ PROYECTO-01-BARBA-ELSA.py
@@ -29,6 +29,32 @@ def login():
                 break
     print(msg)
 
+def init_values():
+    # Taking the id_product column from lifestore_sales list
+    sales_list = []
+    for s in lifestore_sales:
+        sale = s[1]
+        sales_list.append(sale)
+
+    searches_list = []
+    for s in lifestore_searches:
+        search = s[1]
+        searches_list.append(search)
+    
+    products_list = {}
+    for e in lifestore_products:
+        id_product = e[0]
+        name_product = e[1]
+        products_list[id_product] = name_product
+
+    return sales_list, searches_list, products_list
+    
+
+def main():
+    sales_list, searches_list, products_list = init_values()
+    sales_2(sales_list, products_list)
+
+
 #------ Define a function for the sales ------
 def sales():
     # Taking the id_product column from lifestore_sales list
@@ -71,12 +97,7 @@ def sales():
 
 #------ Define a function for the searches ------
 ### This section is pretty much like the sales function but using the lifestore_searches list
-def searches():
-    searches_list = []
-    for s in lifestore_searches:
-        search = s[1]
-        searches_list.append(search)
-
+def searches(searches_list):
     searches_dic = {}
     for i in searches_list:
         if i in searches_dic:
@@ -104,5 +125,49 @@ def searches():
     print("The ten most searched products are: ")
     print(*final_most_searched, sep='\n')
 
+#------ Define another function for sales
+def sales_2(sales_list, products_list):
+    results_per_item = {}
+    category_dict = {}
+
+    # Creating nested dictionaries for the categories
+    for p in lifestore_products:
+        id_product = p[0]
+        category = p[3]
+        results_per_item[id_product] = 0
+        if category not in category_dict:
+            category_dict[category] = {}
+            category_dict[category][id_product] = 0
+        else:
+            category_dict[category][id_product] = 0
+    
+    for s in sales_list:
+        results_per_item[s] = results_per_item[s] + 1
+    
+    # Connecting the number of sales dictionary with the category dictionary
+    #result = {}
+    for e in category_dict.items():
+        category_label = e[0]
+        category_dict_int = e[1]
+        for v in category_dict_int.items():
+            key = v[0]
+            category_dict_int[key] = results_per_item[key]
+        # Sorting from mayor to minor the intern dictionary
+        category_dict_int = dict(sorted(category_dict_int.items(), key= lambda x:x[1]))
+        # Set new values for final dictionary that contains the sorted data
+        #result[category_label] = category_dict_int
+
+        ####
+        i = 0
+        print("Categoria: " + category_label)
+        for v in category_dict_int.items():
+            if i == 5:
+                break
+            id_product = v[0]
+            r_per_item = v[1]
+            print(f"'{products_list[id_product][:20]}...' has been bought {r_per_item} times")
+            i += 1
+    #print(result)
+
 if __name__ == "__main__":
-    sales()
+    main()
